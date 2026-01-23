716681722679114b0a1fdd393196c761e805ef36
Jimena Miguez <jimeblue69@gmail.com>
2026-01-23 11:11:23 +0100

    add animated hero img

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="mt-4 py-2 bg-gray-100">
+  <footer class="py-2 bg-gray-100">
     <div class="container space-x-2 py-4" />
   </footer>
 </template>
--- app/components/base/Hero.vue
+++ app/components/base/Hero.vue
@@ -1,13 +1,14 @@
 <template>
   <section
     id="hero"
-    class="relative mb-10 overflow-hidden bg-black"
+    class="relative pb-28 pt-20 lg:pt-16 overflow-hidden bg-black"
   >
-    <div class="container mx-auto pt-20 lg:pt-16">
+    <div class="container mx-auto">
       <motion.div
         :initial="{ opacity: 0, scale: 0 }"
         :animate="{ opacity: 1, scale: 1 }"
         :transition="{
+          delay: 0.1,
           duration: 0.4,
           scale: { type: 'spring', visualDuration: 0.4, bounce: 0.5 },
         }"
@@ -16,20 +17,47 @@
         <PhraseLogo class="w-full" />
       </motion.div>
       <div class="mx-auto mt-4 h-1 w-20 bg-primary-500" />
-    </div>
-    <div class="text-white">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus debitis tempore, libero rem saepe, eos eaque sit ipsa sunt ratione aperiam reprehenderit provident praesentium nisi illum voluptas aspernatur, architecto accusantium! Minima, quae voluptatum. Debitis perspiciatis asperiores laudantium. Ipsa repellat ex quae possimus quasi quibusdam dolores delectus aspernatur dolor mollitia deserunt, adipisci laudantium praesentium quaerat placeat perspiciatis natus. In, adipisci facilis!</p>
+
+      <motion.img
+        :src="image"
+        alt="ROTLM Band"
+        class="mx-auto mt-12 w-full max-w-4xl rounded-xl will-change-transform"
+        :initial="imageInitial"
+        :animate="imageAnimate"
+        :transition="imageTransition"
+      />
     </div>
   </section>
 </template>
 
 <script setup>
+import { usePreferredReducedMotion } from '@vueuse/core'
 import { motion } from 'motion-v'
 import PhraseLogo from '~/assets/svg/phrase_logo_white.svg?component'
 
 const props = defineProps({
-  title: { type: String, default: null },
-  image: { type: String, default: '/images/hero.jpg' },
+  image: { type: String, default: '/images/ROTLM_hero.png' },
 })
-const { t } = useI18n()
+
+const reduced = usePreferredReducedMotion()
+
+const imageInitial = reduced.value === 'reduce'
+  ? { opacity: 1, y: 0, rotate: -5 }
+  : {
+      opacity: 0,
+      y: 100,
+      rotate: -6,
+      scale: 0.9,
+    }
+
+const imageAnimate = { opacity: 1, y: 0, rotate: -5, scale: 1 }
+
+const imageTransition = reduced.value === 'reduce'
+  ? { duration: 0 }
+  : {
+      type: 'spring',
+      stiffness: 200,
+      damping: 15,
+      mass: 1,
+    }
 </script>
