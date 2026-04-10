4036681b8350cf1b5c4ae5c0a010a88bd1aa56d8
Jimena Miguez <jimeblue69@gmail.com>
2026-04-10 16:16:23 +0200

    fix merch hero

--- app/components/base/HeroMerch.vue
+++ app/components/base/HeroMerch.vue
@@ -4,26 +4,28 @@
       v-if="merchHeroImage"
       :src="sanityImageUrl(merchHeroImage.url)"
       :alt="merchHeroImage.alt || ''"
-      class="absolute inset-0 w-full h-full object-fill object-top"
+      class="w-full block"
     >
-    <div class="container mx-auto relative z-10">
-      <motion.div
-        :initial="{ opacity: 0, scale: 0 }"
-        :animate="{ opacity: 1, scale: 1 }"
-        :transition="{
-          delay: 0.1,
-          duration: 0.4,
-          scale: { type: 'spring', visualDuration: 0.4, bounce: 0.5 },
-        }"
-        class="mx-auto w-full max-w-4xl mt-4 lg:mt-10"
-      >
-        <img
-          v-if="merchImage"
-          :src="sanityImageUrl(merchImage.url)"
-          :alt="merchImage.alt || 'Raiders of the Lost Missile'"
-          class="w-full"
+    <div class="absolute inset-0 flex items-center justify-center z-10">
+      <div class="container mx-auto">
+        <motion.div
+          :initial="{ opacity: 0, scale: 0 }"
+          :animate="{ opacity: 1, scale: 1 }"
+          :transition="{
+            delay: 0.1,
+            duration: 0.4,
+            scale: { type: 'spring', visualDuration: 0.4, bounce: 0.5 },
+          }"
+          class="mx-auto w-full max-w-4xl"
         >
-      </motion.div>
+          <img
+            v-if="merchImage"
+            :src="sanityImageUrl(merchImage.url)"
+            :alt="merchImage.alt || 'Raiders of the Lost Missile'"
+            class="w-full"
+          >
+        </motion.div>
+      </div>
     </div>
   </section>
 </template>
