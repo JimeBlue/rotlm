c122ed9d8ab2201909f34fceec8223d2fced0a47
Jimena Miguez <jimeblue69@gmail.com>
2026-09-11 10:46:50 +0200

    set the html lang attribute on the contact layout

--- app/layouts/contact.vue
+++ app/layouts/contact.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="flex min-h-screen flex-1 flex-col font-body bg-black">
-    <AppHeader />
-    <main id="main-content" class="flex flex-1 flex-col">
-      <slot />
-    </main>
-    <ContactFooter />
-  </div>
+  <Html :lang="head.htmlAttrs.lang">
+    <Body>
+      <div class="flex min-h-screen flex-1 flex-col font-body bg-black">
+        <AppHeader />
+        <main id="main-content" class="flex flex-1 flex-col">
+          <slot />
+        </main>
+        <ContactFooter />
+      </div>
+    </Body>
+  </Html>
 </template>
+
+<script setup>
+const head = useLocaleHead()
+</script>
